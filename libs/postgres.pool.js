26317bb5ef340d7c7c5ec44290acfe7f0e334f73
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-22 18:15:34 -0300

    URI added for connection

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = require('../config');
 
-const pool = new Pool({
-	host: DB_HOST,
-	port: DB_PORT,
-	user: DB_USER,
-	password: DB_PASS,
-	database: DB_NAME
-})
+const USER = encodeURIComponent(DB_USER);
+const PASSWORD = encodeURIComponent(DB_PASS);
+const URI = `postgresql://${USER}:${PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
