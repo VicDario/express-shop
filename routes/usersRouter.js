10f9a18d62b4816e4495038af409e2c06734be30
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-22 17:05:22 -0300

    Testing db

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,17 +1,11 @@
 const express = require('express');
+const UserService = require('../services/userService');
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-	const { limit, offset} = req.query;
-	if (!limit || !offset) {
-		res.status(400).json({
-			error: 'Bad request'
-		});
-	}
-	res.json({
-		limit,
-		offset
-	});
+const userService = new UserService();
+router.get('/users', async (req, res) => {
+	const users = await userService.find();
+	res.json(users);
 });
 
 module.exports = router;
