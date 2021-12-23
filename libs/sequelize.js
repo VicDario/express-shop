ae8bda47a8444b59fbf96ba656b8c0c653c5e0c3
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-23 15:09:15 -0300

    Set models

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = require('../config');
+const { setUpModels } = require('../db/models');
 
 const USER = encodeURIComponent(DB_USER);
 const PASSWORD = encodeURIComponent(DB_PASS);
@@ -9,5 +10,9 @@ const sequelize = new Sequelize(URI, {
 	dialect: 'postgres',
 	loggin: true,
 });
+
+setUpModels(sequelize);
+
+sequelize.sync();
 
 module.exports = sequelize;
