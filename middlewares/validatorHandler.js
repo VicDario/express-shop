37041d1fa635110c5095c98f2555d4864ff5eceb
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-23 15:20:15 -0300

    console.log deleted

--- middlewares/validatorHandler.js
+++ middlewares/validatorHandler.js
@@ -3,10 +3,8 @@ const boom = require('@hapi/boom');
 function validatorHandler(schema, property) {
 	return ((req, res, next) => {
 		const data = req[property];
-		console.log(schema, property);
 		const { error } = schema.validate(data, { abortEarly: false });
 		if (error) {
-			console.log('mano');
 			next(boom.badRequest(error));
 		}
 		next();
