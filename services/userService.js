48dea893a988379503b6c8bd5b9d85162eecb613
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-23 17:00:50 -0300

    Functions completed on service

--- services/userService.js
+++ services/userService.js
@@ -9,7 +9,8 @@ class UserService {
 	}
 
 	async create(data) {
-		return data;
+		const newUser = await models.User.create(data);
+		return newUser;
 	}
 
 	async find() {
@@ -18,18 +19,21 @@ class UserService {
 	}
 
 	async findOne(id) {
-		return { id };
+		const user = await models.User.findByPk(id);
+		if(!user) throw boom.notFound('User not found');
+		return user	;
 	}
 
 	async update(id, changes) {
-		return {
-			id,
-			changes,
-		};
+		const user = await this.findOne(id);
+		const result = await user.update(changes);
+		return result;
 	}
 
 	async delete(id) {
-		return { id };
+		const user = await this.findOne(id);
+		const result = await user.destroy();
+		return result;
 	}
 }
 
