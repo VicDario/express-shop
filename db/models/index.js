e878f85ca6cfaccdbc0dc6b8679f17d597de9116
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-29 12:57:59 -0300

    Added relationship customer-user

--- db/models/index.js
+++ db/models/index.js
@@ -4,6 +4,8 @@ const { Customer, CustomerSchema } = require('./customerModel');
 function setUpModels(sequelize) {
 	User.init(UserSchema, User.config(sequelize));
 	Customer.init(CustomerSchema, Customer.config(sequelize));
+
+	Customer.associate(sequelize.models);
 }
 
 module.exports = { setUpModels };
