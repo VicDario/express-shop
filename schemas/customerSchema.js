ebb2fefd27a056b2a150393515508f6e741c14d2
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-29 13:06:34 -0300

    Changes

--- schemas/customerSchema.js
+++ schemas/customerSchema.js
@@ -3,30 +3,31 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const name = Joi.string().min(2).max(20);
 const lastName = Joi.string();
-const phone =  Joi.string();
+const phone = Joi.string();
 const userId = Joi.number().integer();
-const email = Joi.string().email();
-const password =  Joi.string();
+/* const email = Joi.string().email();
+const password = Joi.string(); */
 
 const getCustomerSchema = Joi.object({
-  id: id.required(),
+	id: id.required(),
 });
 
 const createCustomerSchema = Joi.object({
-  name: name.required(),
-  lastName: lastName.required(),
-  phone: phone.required(),
-  user: Joi.object({
-    email: email.required(),
-    password: password.required()
-  })
+	name: name.required(),
+	lastName: lastName.required(),
+	phone: phone.required(),
+	userId: userId.required(),
 });
 
 const updateCustomerSchema = Joi.object({
-  name,
-  lastName,
-  phone,
-  userId
+	name,
+	lastName,
+	phone,
+	userId,
 });
 
-module.exports = { getCustomerSchema, createCustomerSchema, updateCustomerSchema };
+module.exports = {
+	getCustomerSchema,
+	createCustomerSchema,
+	updateCustomerSchema,
+};
