6d5de73878d81d1ebfa2050efae9b0216fe87955
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-29 12:02:51 -0300

    customer routes added

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const productsRouter = require('./productsRouter');
 const usersRouter = require('./usersRouter');
+const customersRouter = require('./customersRouter');
 
 function routerAPI (app) {
 	const router = express.Router();
 	app.use('/api/v1', router);
 	router.use('/products', productsRouter);
 	router.use('/user', usersRouter);
+	router.use('/customer', customersRouter);
 }
 
 module.exports = routerAPI;
