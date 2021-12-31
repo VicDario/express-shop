cda80285a90fb8a6f8a4ba7f7592b8face88f1f5
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-31 14:06:59 -0300

    relation many to many added

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,7 @@ const { Customer, CustomerSchema } = require('./customerModel');
 const { Product, ProductSchema } = require('./productModel');
 const { Category, CategorySchema } = require('./categoryModel');
 const { Order, OrderSchema } = require('./orderModel');
+const { OrderProduct, OrderProductSchema } = require('./order-productModel');
 
 function setUpModels(sequelize) {
 	User.init(UserSchema, User.config(sequelize));
@@ -10,12 +11,14 @@ function setUpModels(sequelize) {
 	Product.init(ProductSchema, Product.config(sequelize));
 	Category.init(CategorySchema, Category.config(sequelize));
 	Order.init(OrderSchema, Order.config(sequelize));
+	OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
 	User.associate(sequelize.models);
 	Customer.associate(sequelize.models);
 	Product.associate(sequelize.models);
 	Category.associate(sequelize.models);
 	Order.associate(sequelize.models);
+	OrderProduct.associate(sequelize.models);
 }
 
 module.exports = { setUpModels };
